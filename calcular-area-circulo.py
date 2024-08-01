65062451499bbaff1774640a4fdd9d0495f04b7f
Brayan Narvaez <brayannsaez77@gmail.com>
2024-08-01 11:43:19 -0500

    Realizamos una calculadora del area de un circulo

---
+++ calcular-area-circulo.py
@@ -0,0 +1,41 @@
+
+
+import math
+
+
+radio = float(input("Por favor, ingrese el radio del círculo: "))
+
+area = math.pi * (radio**2)
+
+print("El área del círculo con radio", radio, "es de:", area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
